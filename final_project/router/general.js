b302c910fb925b972a88b9e0372b8ce1f907a366
GERALD IZUCHUKWU <39552823+Gerald-Izuchukwu@users.noreply.github.com>
2023-06-13 13:26:45 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,149 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (!isValid(username)) {
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});
+    }
+  }
+  return res.status(404).json({message: "Unable to register user."});
+
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
+public_users.get('/reviews/:isbn', function (req, res) {
+    const isbn = parseInt(req.params.isbn);
+  
+    getBookReviews(isbn)
+      .then(reviews => {
+        if (reviews.length === 0) {
+          return res.status(404).json({ message: 'No reviews found for the book' });
+        }
+        res.json(reviews);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+      });
+  });
+  
+  function getBookReviews(isbn) {
+    return new Promise((resolve, reject) => {
+      // Assuming `books` is accessible here
+  
+      const book = books[isbn];
+      if (!book) {
+        reject('Book not found');
+      } else {
+        const reviews = book.reviews;
+        resolve(reviews);
+      }
+    });
+  }
+  
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = parseInt(req.params.isbn);
+    
+    getBookByISBN(isbn)
+      .then(book => {
+        res.status(200).json({
+          message: "Get books by ISBN",
+          book
+        });
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({
+          message: "Internal server error"
+        });
+      });
+  });
+  
+  function getBookByISBN(isbn) {
+    return new Promise((resolve, reject) => {
+      // Assuming `books` is accessible here
+  
+      const book = books[isbn];
+  
+      if (book) {
+        resolve(book);
+      } else {
+        reject("Book not found");
+      }
+    });
+  }
+  
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+  
+    getBooksByAuthor(author)
+      .then(authorBooks => {
+        if (authorBooks.length === 0) {
+          return res.status(404).json({ message: 'No books found for the author' });
+        }
+        res.json(authorBooks);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+      });
+  });
+  
+  function getBooksByAuthor(author) {
+    return new Promise((resolve, reject) => {
+      // Assuming `books` is accessible here
+  
+      const authorBooks = Object.values(books).filter(book => book.author.toLowerCase() === author.toLowerCase());
+  
+      resolve(authorBooks);
+    });
+  }
+  
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title;
+  
+    getBooksByTitle(title)
+      .then(titleBooks => {
+        if (titleBooks.length === 0) {
+          return res.status(404).json({ message: 'No books found for the title' });
+        }
+        res.json(titleBooks);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+      });
+  });
+  
+  function getBooksByTitle(title) {
+    return new Promise((resolve, reject) => {
+      // Assuming `books` is accessible here
+  
+      const titleBooks = Object.values(books).filter(book => book.title.toLowerCase() === title.toLowerCase());
+  
+      resolve(titleBooks);
+    });
+  }
+  
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = parseInt(req.params.isbn);
+
+  
+
+  return res.json(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
